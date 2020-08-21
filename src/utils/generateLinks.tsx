66440a40a2aaac0a14021e759f814df2164e0fc2
Michael Armani <michaelarmani00@gmail.com>
2020-08-21 21:30:56 +0200

    Updated navbar links + external links checker for Gatsby Link API

--- src/utils/generateLinks.tsx
+++ src/utils/generateLinks.tsx
@@ -6,9 +6,26 @@ import { useNavbarlinks } from '../queries/useNavbarLinks';
 export const generateLinks = (): any => {
   const links = useNavbarlinks();
 
-  return links.map(({ path, name, id }) => (
-    <Link key={id} to={path}>
-      {name}
-    </Link>
-  ));
+  return links.map(({ path, name, id }) => {
+    const isInternal = /^\/(?!\/)/.test(path);
+
+    if (isInternal) {
+      return (
+        <Link key={id} to={path}>
+          {name}
+        </Link>
+      );
+    }
+
+    return (
+      <a
+        key={id}
+        href={path}
+        target="_blank"
+        rel="nofollow noopener noreferrer"
+      >
+        {name}
+      </a>
+    );
+  });
 };
